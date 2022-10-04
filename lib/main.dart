cd2fd045d69be4575b9e30c0791b4bd858d7967b
aditgocendra <gocendra123@gmail.com>
2022-10-05 00:20:31 +0700

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:check_postage_app/app/core/bindings/initial_binding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    GetMaterialApp(
-      title: "Application",
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+    return GetMaterialApp(
+      title: "Shipp",
       initialBinding: InitializeBinding(),
-      initialRoute: AppPages.INITIAL,
+      initialRoute: AppPages.initial,
       getPages: AppPages.routes,
-    ),
-  );
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme.apply(
+                bodyColor: Colors.black87,
+              ),
+        ),
+      ),
+    );
+  }
 }
