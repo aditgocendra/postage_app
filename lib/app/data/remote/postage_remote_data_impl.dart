8ec1b84e055ce1df41431274d33b531d332ac06c
aditgocendra <gocendra123@gmail.com>
2022-10-06 14:41:09 +0700

    refactor with base remote

--- lib/app/data/remote/postage_remote_data_impl.dart
+++ lib/app/data/remote/postage_remote_data_impl.dart
@@ -1,55 +1,11 @@
 import 'package:check_postage_app/app/core/error/error_model.dart';
+import 'package:check_postage_app/app/data/remote/base_remote.dart';
 import 'package:check_postage_app/app/data/remote/postage_remote_data.dart';
 import 'package:either_dart/either.dart';
 import 'package:http/http.dart' as http;
-import '../../core/keys/api_key.dart';
 
-class PostageRemoteDataImpl implements PostageRemoteDataSource {
-  Future<Either<ErrorModel, http.Response>> getRequest(String endPoint) async {
-    final response = await http.get(
-      Uri.parse(endPoint),
-      headers: {"key": apiKeyRajaOngkir},
-    );
-
-    switch (response.statusCode) {
-      case 200:
-        return Right(response);
-      default:
-        return Left(
-          ErrorModel(
-            key: AppError.badRequest,
-            message: "Bad Request",
-          ),
-        );
-    }
-  }
-
-  Future<Either<ErrorModel, http.Response>> postRequest(
-    String endPoint,
-    Object body,
-  ) async {
-    final response = await http.post(
-      Uri.parse(endPoint),
-      headers: {
-        "key": apiKeyRajaOngkir,
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: body,
-    );
-
-    switch (response.statusCode) {
-      case 200:
-        return Right(response);
-      default:
-        return Left(
-          ErrorModel(
-            key: AppError.badRequest,
-            message: "Bad Request",
-          ),
-        );
-    }
-  }
-
+class PostageRemoteDataImpl extends BaseRemote
+    implements PostageRemoteDataSource {
   @override
   Future<Either<ErrorModel, http.Response>> getDataProvince() async {
     return await getRequest("https://api.rajaongkir.com/starter/province");
