a5d6677e6c3c6a8d5dc231f5b06c5e5009973d84
aditgocendra <gocendra123@gmail.com>
2022-12-10 18:49:11 +0700

    refactor

--- lib/app/data/repositories/postage_repository_impl.dart
+++ lib/app/data/repositories/postage_repository_impl.dart
@@ -1,52 +1,48 @@
-import 'dart:convert';
-import 'package:check_postage_app/app/data/models/city_model.dart';
-import 'package:check_postage_app/app/data/models/courier_service_model.dart';
-import 'package:check_postage_app/app/data/models/province_model.dart';
-import 'package:check_postage_app/app/data/remote/postage_remote_data.dart';
-import 'package:check_postage_app/app/data/repositories/postage_repository.dart';
-import 'package:either_dart/either.dart';
-import 'package:get/get.dart';
+import "package:either_dart/either.dart";
 import '../../core/error/failure.dart';
+import '../models/city_model.dart';
+import '../models/courier_service_model.dart';
+import '../models/province_model.dart';
+import '../remote/postage_remote_data.dart';
+import 'postage_repository.dart';
 
 class PostageRepositoryImpl implements PostageRepository {
-  final remoteSourceData = Get.find<PostageRemoteDataSource>();
+  PostageRemoteData postageRemoteData;
+
+  PostageRepositoryImpl({
+    required this.postageRemoteData,
+  });
 
   @override
   Future<Either<Failure, List<ProvinceModel>>> getProvinces() async {
-    List<ProvinceModel> province = [];
     // Get data province
-    final value = await remoteSourceData.getDataProvince();
+    final result = await postageRemoteData.getDataProvince();
 
     // Return Request Failed
-    if (value.isLeft) {
-      return Left(value.left);
+    if (result.isLeft) {
+      return Left(result.left);
     }
 
     // Return Request Success
-    var res = value.right;
-    province = ProvinceModel.fromJsonList(
-        jsonDecode(res.body)["rajaongkir"]["results"]);
-
-    return Right(province);
+    return Right(ProvinceModel.fromJsonList(
+      result.right["rajaongkir"]["results"],
+    ));
   }
 
   @override
   Future<Either<Failure, List<CityModel>>> getCity(String idProv) async {
-    List<CityModel> city = [];
     // Get data province
-    final value = await remoteSourceData.getDataCity(idProv);
+    final result = await postageRemoteData.getDataCity(idProv);
 
     // Return Request Failed
-    if (value.isLeft) {
-      return Left(value.left);
+    if (result.isLeft) {
+      return Left(result.left);
     }
 
     // Return Request Success
-    var res = value.right;
-    city =
-        CityModel.fromJsonList(jsonDecode(res.body)["rajaongkir"]["results"]);
-
-    return Right(city);
+    return Right(CityModel.fromJsonList(
+      result.right["rajaongkir"]["results"],
+    ));
   }
 
   @override
@@ -56,7 +52,7 @@ class PostageRepositoryImpl implements PostageRepository {
     String weight,
     String courier,
   ) async {
-    final value = await remoteSourceData.getDataPostage(
+    final result = await postageRemoteData.getDataPostage(
       origin,
       destination,
       weight,
@@ -64,13 +60,15 @@ class PostageRepositoryImpl implements PostageRepository {
     );
 
     // Return Request Failed
-    if (value.isLeft) {
-      return Left(value.left);
+    if (result.isLeft) {
+      return Left(result.left);
     }
 
     // // Return Request Success
-    var res = value.right;
-    var result = jsonDecode(res.body)['rajaongkir']['results'][0]['costs'];
-    return Right(CourierServiceModel.fromJsonList(result));
+    return Right(
+      CourierServiceModel.fromJsonList(
+        result.right['rajaongkir']['results'][0]['costs'],
+      ),
+    );
   }
 }
