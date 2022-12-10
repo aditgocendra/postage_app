b60afd17d943afbeada963c6360a2a5d1919c04e
aditgocendra <gocendra123@gmail.com>
2022-12-10 18:45:00 +0700

    update

--- lib/app/core/bindings/repository_binding.dart
+++ lib/app/core/bindings/repository_binding.dart
@@ -2,11 +2,15 @@ import 'package:check_postage_app/app/data/repositories/postage_repository.dart'
 import 'package:check_postage_app/app/data/repositories/postage_repository_impl.dart';
 import 'package:get/get.dart';
 
+import '../../data/remote/postage_remote_data.dart';
+import '../../data/remote/postage_remote_data_impl.dart';
+
 class RepositoryBindings implements Bindings {
   @override
   void dependencies() {
+    PostageRemoteData postageRemoteData = PostageRemoteDataImpl();
     Get.lazyPut<PostageRepository>(
-      () => PostageRepositoryImpl(),
+      () => PostageRepositoryImpl(postageRemoteData: postageRemoteData),
     );
   }
 }
